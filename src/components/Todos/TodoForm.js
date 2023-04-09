9b27b112c7968a5fdff32002677668b32bdda058
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-04-09 15:02:34 +0300

    Todo App

---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import styles from './TodoForm.module.css';
+import Button from '../UI/Button';
+
+const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState('');
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    addTodo(text);
+    setText('');
+  };
+
+  return (
+    <form className={styles.form} action="" onSubmit={onSubmitHandler}>
+      <input
+        className={styles.input}
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        type="text"
+        placeholder="Enter new todo"
+      />
+      <Button className={styles.btnSubmit} title="Submit" type="submit">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default TodoForm;
